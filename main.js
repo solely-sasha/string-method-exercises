e0d0ef253ea68388c8e8273be847e59ae6ca067c
sasha sims <solely.sashasims@gmail.com>
2022-11-08 20:34:38 -0500

    set up

---
+++ main.js
@@ -0,0 +1,9 @@
+// 1. Write a function that takes a string as a parameter and returns the same string in all capital letters followed by all lowercase letters.
+
+// 2. Write a function that takes a string as a parameter and returns a number that is half the string's length, rounded down.
+
+
+// 3. Write a function that uses slice() and the other functions you've written to return the first half of the given string.
+
+// 4. Write a function that takes a string as a parameter and returns that string where the first half is capitalized, and the second half is lowercase.
+
